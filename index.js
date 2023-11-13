8b0427a7600f3c10e9afad1d47e9dac8119829f2
changwoolab <cwyoo01@naver.com>
2023-11-13 11:29:32 +0900

    몇명 뽑을지 추가

--- index.js
+++ index.js
@@ -2,28 +2,38 @@ import fs from "fs";
 import { stringify } from "csv-stringify";
 import inquirer from "inquirer";
 
+const QUESTIONS = [
+  {
+    type: "input",
+    name: "filepath",
+    message: "랜덤으로 뽑을 파일의 경로를 입력해주세요. \n(예시: ./20명추첨)",
+    validate: (input) => input.trim().length !== 0,
+    transformer: (input) => input.trim(),
+  },
+  {
+    type: "input",
+    name: "howmany",
+    message: "몇 명을 추첨하시겠습니까?",
+    validate: (input) => parseInt(input.trim()) > 0,
+    transformer: (input) => input.trim(),
+  },
+  {
+    type: "input",
+    name: "outputFilepath",
+    message: "저장될 csv 파일의 이름을 입력해주세요. \n(예시: 20명추첨.csv)",
+    transformer: (input) => input.trim(),
+  },
+];
+
 async function main() {
-  const questions = [
-    {
-      type: "input",
-      name: "filepath",
-      message: "랜덤으로 뽑을 파일의 경로를 입력해주세요. \n(예시: ./20명추첨)",
-      validate: (input) => input.trim().length !== 0,
-      transformer: (input) => input.trim(),
-    },
-    {
-      type: "input",
-      name: "outputFilepath",
-      message: "저장될 csv 파일의 이름을 입력해주세요. \n(예시: 20명추첨.csv)",
-      transformer: (input) => input.trim(),
-    },
-  ];
-
   await showLogo();
-  const answer = await inquirer.prompt(questions);
 
-  const files = readFileAndSerialize(answer.filepath);
-  const result = getRandom(files, 40);
+  const { filepath, howmany, outputFilepath } = await inquirer.prompt(
+    QUESTIONS
+  );
+
+  const files = readFileAndSerialize(filepath);
+  const result = getRandom(files, parseInt(howmany));
 
   const serializedResult = result.reduce(
     (acc, curr) => {
@@ -35,7 +45,7 @@ async function main() {
 
   stringify(serializedResult, (err, output) => {
     if (err) throw err;
-    fs.writeFileSync(answer.outputFilepath, output);
+    fs.writeFileSync(outputFilepath, output);
   });
 }
 
@@ -55,11 +65,11 @@ async function showLogo() {
 }
 
 function getRandom(arr, n) {
-  const result = new Array(n),
+  let result = new Array(n),
     len = arr.length,
     taken = new Array(len);
   if (n > len)
-    throw new RangeError("getRandom: more elements taken than available");
+    throw new RangeError("뽑을 수 있는 개수보다 뽑으려는 개수가 많습니다.");
   while (n--) {
     const x = Math.floor(Math.random() * len);
     result[n] = arr[x in taken ? taken[x] : x];
